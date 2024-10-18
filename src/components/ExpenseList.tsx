3534a9826dc952d99d79e8f490279ce8e1b55c48
sergio.pena <sergio.pena.orozco@gmail.com>
2024-10-18 01:31:29 -0600

    add fiter by category

--- src/components/ExpenseList.tsx
+++ src/components/ExpenseList.tsx
@@ -9,14 +9,16 @@ export default function ExpenseList() {
 
     const isEmpty = useMemo(() => state.expenses.length === 0, [state.expenses])
 
+    const filteredExpenses = state.currentCategory ? state.expenses.filter(expense => expense.category === state.currentCategory) : state.expenses
+
     return (
-        <div className="mt-10">
+        <div className="mt-10 bg-white shadow-lg rounded-lg p-5">
             {isEmpty 
                 ? <p className="text-gray-600 text-2xl font-bold">No Hay Gastos</p>
                 : (
                     <>
                         <p className="text-gray-600 text-2xl font-bold">Listado de Gastos</p>
-                        {state.expenses.map(expense => (
+                        {filteredExpenses.map(expense => (
                             <ExpenseDetail
                                 key={expense.id}
                                 expense={expense}
