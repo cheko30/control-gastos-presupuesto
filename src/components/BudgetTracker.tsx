78dc07b34aeebf6307514a9a8912746beebc5b9e
sergio.pena <sergio.pena.orozco@gmail.com>
2024-10-18 00:58:23 -0600

    add dashboard functionality

--- src/components/BudgetTracker.tsx
+++ src/components/BudgetTracker.tsx
@@ -1,33 +1,37 @@
+import { useBudget } from "../hooks/useBudget";
 import AmountDisplay from "./AmountDisplay";
 
 export default function BudgetTracker() {
-  return (
-    <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
-        <div className="flex justify-center">
-            <img src="/grafico.jpg" alt="Grafica de Gastos" />
-        </div>
 
-        <div className="flex flex-col justify-center items-center gap-8">
-            <button
-                type="button"
-                className="bg-pink-600 w-full p-2 text-white uppercase font-bold rounded-lg"
-            >
-                Resetear App
-            </button>
+    const { state, totalExpenses, remainingBudget } = useBudget()
+    
+    return (
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
+            <div className="flex justify-center">
+                <img src="/grafico.jpg" alt="Grafica de Gastos" />
+            </div>
 
-            <AmountDisplay
-                label="Presupuesto"
-                amount={300}
-            />
-            <AmountDisplay
-                label="Disponible"
-                amount={200}
-            />
-            <AmountDisplay
-                label="Gastado"
-                amount={100}
-            />
+            <div className="flex flex-col justify-center items-center gap-8">
+                <button
+                    type="button"
+                    className="bg-pink-600 w-full p-2 text-white uppercase font-bold rounded-lg"
+                >
+                    Resetear App
+                </button>
+
+                <AmountDisplay
+                    label="Presupuesto"
+                    amount={state.budget}
+                />
+                <AmountDisplay
+                    label="Disponible"
+                    amount={remainingBudget}
+                />
+                <AmountDisplay
+                    label="Gastado"
+                    amount={totalExpenses}
+                />
+            </div>
         </div>
-    </div>
-  )
+    )
 }
--- src/components/ExpenseForm.tsx
+++ src/components/ExpenseForm.tsx
@@ -16,12 +16,14 @@ export default function ExpenseForm() {
     })
 
     const [error, setError] = useState("")
-    const { dispatch, state } = useBudget()
+    const [previousAmount, setPreviousAmount] = useState(0)
+    const { dispatch, state, remainingBudget } = useBudget()
 
     useEffect(() => {
         if(state.editingId) {
             const editingExpense = state.expenses.filter(currentExpense => currentExpense.id === state.editingId)[0]
             setExpense(editingExpense)
+            setPreviousAmount(editingExpense.amount)
         }
 
     }, [state.editingId])
@@ -52,6 +54,12 @@ export default function ExpenseForm() {
             return
         }
 
+        // Validar monto disponible
+        if(expense.amount - previousAmount > remainingBudget) {
+            setError("¡El gasto se sale del presupuesto!")
+            return
+        }
+
         // Agregar o actualizar un gasto
         if(state.editingId) {
             dispatch({type: "update-expense", payload: {expense: {id: state.editingId, ...expense}}})
@@ -66,6 +74,7 @@ export default function ExpenseForm() {
             category: "",
             date: new Date()
         })
+        setPreviousAmount(0)
     }
 
   return (
